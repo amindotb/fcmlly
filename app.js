4097dad46ce0e00e1b7fd99cfea90fdaa7badec5
Amin Bagheri <amindotb@gmail.com>
2020-10-20 12:28:03 +0330

    version 1.2

--- app.js
+++ app.js
@@ -67,12 +67,3 @@ class Fcmlly {
     //     return await this._send(req);
     // }
 }
-
-const YOUR_APIKEY =  "AAAAOkuECnI:APA91bG_dHSxyVC_04_y0TSPwCtkvYRAd9aHU8xKbZI3QH-YLJxH2yHehLM12QSaW2DSHr6ABGZZr7CVR2jkOy66-EGSbIdrvKTofYSyxD_Idj8OoW-daZ6SLxnJs_mHbqHVUM3r0luq"
-const fcmlly = new Fcmlly(YOUR_APIKEY);
-
-
-    const result = await fcmlly.pushToUser('eGq0OcR2TwCn1Ez5bgbnCi:APA91bES5pD0z_k29jtf1e-jnAG-wNBATtODxFYC6te06QN8HEMsU_3vMGKnXZCSuzpCTu7KVt59jr_JsDcxag6CoKXoAdFtXGWQR-91xdEIQpP1M_GAe0BrCzl9KBsM27k1kIjh1mev',
-    'some title', 'some body')
-    console.log(result)
-
