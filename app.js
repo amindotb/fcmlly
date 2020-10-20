6feb4c1ab35dc8938ac79fee2909bb9ab900e691
Amin Bagheri <amindotb@gmail.com>
2020-10-20 12:25:43 +0330

    v 1.1 final

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ const unirest = require('unirest');
 const GOOGLE_FCM_ENDPOINT = 'https://fcm.googleapis.com/fcm/send';
 
 class Fcmlly {
-    APIKey = null;
+
+    constructor(APIKEY) {
+        this.APIKey = APIKEY;
+    }
 
     async _send(body) {
         return new Promise((resolve,reject)=>{
-            if(!self.APIKey) 
+            if(!this.APIKey) 
                 throw Error('You must provide the APIKEY for your firebase applications.');
-            else if(typeof self.APIKey != 'string') 
+            else if(typeof this.APIKey != 'string') 
                 throw Error('Your API key is incorrect');
 
             unirest
             .post(GOOGLE_FCM_ENDPOINT)
-            .headers({'Content-Type': 'application/json','Authorization': `key=${ self.APIKey }`})
+            .headers({'Content-Type': 'application/json','Authorization': `key=${ this.APIKey }`})
             .send(body)
             .then((response) => {
-                resolve(response);
+                resolve(response.raw_body);
             })
             .catch((error) => {
                 reject(error);
@@ -38,19 +41,6 @@ class Fcmlly {
         return await this._send(req);
     }
 
-    async pushToAll(title,body,data=null,click_action=null) {
-        const req = { notification: {
-            title,
-            body,
-            click_action
-          },
-            data,
-            to : `/`
-        };
-
-        return await this._send(req);
-    }
-
     async pushToTopic(topic,title,body,data=null,click_action=null) {
         const req = { notification: {
             title,
@@ -64,16 +54,25 @@ class Fcmlly {
         return await this._send(req);
     }
 
-    async pushToUsers(ids,title,body,data=null,click_action=null) {
-        const req = { notification: {
-            title,
-            body,
-            click_action
-          },
-            data,
-            to : id
-        };
+    // async pushToUsers(ids,title,body,data=null,click_action=null) {
+    //     const req = { notification: {
+    //         title,
+    //         body,
+    //         click_action
+    //       },
+    //         data,
+    //         to : id
+    //     };
 
-        return await this._send(req);
-    }
+    //     return await this._send(req);
+    // }
 }
+
+const YOUR_APIKEY =  "AAAAOkuECnI:APA91bG_dHSxyVC_04_y0TSPwCtkvYRAd9aHU8xKbZI3QH-YLJxH2yHehLM12QSaW2DSHr6ABGZZr7CVR2jkOy66-EGSbIdrvKTofYSyxD_Idj8OoW-daZ6SLxnJs_mHbqHVUM3r0luq"
+const fcmlly = new Fcmlly(YOUR_APIKEY);
+
+
+    const result = await fcmlly.pushToUser('eGq0OcR2TwCn1Ez5bgbnCi:APA91bES5pD0z_k29jtf1e-jnAG-wNBATtODxFYC6te06QN8HEMsU_3vMGKnXZCSuzpCTu7KVt59jr_JsDcxag6CoKXoAdFtXGWQR-91xdEIQpP1M_GAe0BrCzl9KBsM27k1kIjh1mev',
+    'some title', 'some body')
+    console.log(result)
+
--- readme.md
+++ readme.md
@@ -1,9 +1,7 @@
 Fcmlly - Integrate with Google cloud messaging easily
 ------------
-Best way to send you push notifications.
+Best way to send your push notifications.
 [![Thanks to brentclouse for this gif](https://cdn.dribbble.com/users/187497/screenshots/9363790/media/34f0f653b8f6fa911bd5ccf8ec9e4bab.gif "Thanks to brentclouse")](https://dribbble.com/brentclouse "Thanks to brentclouse for this gif")
-
-
 I accept any contributes with open arms \\./
 
 ### Instalation
@@ -15,24 +13,29 @@ npm i fcmlly
 ### Usage
 ------------
  ```
- const Fcmlly = requre('fcmlly');
- const fcmlly = new Fcmlly();
+ const Fcmlly = require('fcmlly');
+ const fcmlly = new Fcmlly('YOUR_APIKEY_HERE'); // Get this key from Firebase console > Settings (page) > Cloud Messaging (tab) > Project credentials (section) > Server Key
  ```
 
 ### Samples
 ------------
 ```
-const res = await fcmlly.pushToUser('user fcm token', 'title', 'body');
+const res = await fcmlly.pushToUser('user fcm token', 'youe title text', 'your body text');
 console.log(res);
 
 or
 
-fcmlly.pushToUser('user fcm token', 'title', 'body').then((res) => { console.log(res) }).catch((err) => { console.error(err) });
+fcmlly.pushToUser('user fcm token', 'youe title text', 'your body text').then((res) => { console.log(res) }).catch((err) => { console.error(err) });
 
 
 
-// console res:
-// 
+// sample console output:
+// {
+//  multicast_id: 76468637435051*****,
+//  success: 1,
+//  failure: 0,
+//  canonical_ids: 0,
+//  results: [ { message_id: '0:1603183795340265%a5e3d8a7a5e*****' } ]
 // }
  ```
 
@@ -42,15 +45,9 @@ fcmlly.pushToUser('user fcm token', 'title', 'body').then((res) => { console.log
 ------------
 * Push to a user
 ```.push('userId', 'title', 'body') ```        
-
-* Push to all
-```.pushToAll('title', 'body') ```        
-
+     
 * Push to all users in a topic
 ```.pushToTopic('topic name', 'title', 'body') ```        
-
-* Push to users
-```.pushToUsers(['userId'], 'title', 'body') ```        
 
 
 
